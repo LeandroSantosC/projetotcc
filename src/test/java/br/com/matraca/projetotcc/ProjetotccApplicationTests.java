76a2c0d91397573d9c60ce9cff52ee0c39850551
Leandro Santos <leandros_cruz@outlook.com>
2024-09-20 09:15:12 -0300

    Iniciando projeto

---
+++ src/test/java/br/com/matraca/projetotcc/ProjetotccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matraca.projetotcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
