e481d8929c9baebd3b5bd359e5bcbd9101b9f6b6
Leandro Santos <leandros_cruz@outlook.com>
2024-10-14 08:30:32 -0300

    começando front

--- src/main/java/br/com/matraca/projetotcc/model/entity/Board.java
+++ src/main/java/br/com/matraca/projetotcc/model/entity/Board.java
@@ -2,14 +2,12 @@ package br.com.matraca.projetotcc.model.entity;
 
 import java.util.List;
 
-import br.com.matraca.projetotcc.model.dto.ButtonRequestDTO;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
