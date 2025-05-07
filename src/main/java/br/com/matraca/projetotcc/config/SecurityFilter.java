9ef2ea2f2eeca10f306cb79081285fc829dc3713
Leandro Santos <leandros_cruz@outlook.com>
2025-05-07 13:44:31 -0300

    Atualizando o securityFilter para retornar o CORS quando der erro

--- src/main/java/br/com/matraca/projetotcc/config/SecurityFilter.java
+++ src/main/java/br/com/matraca/projetotcc/config/SecurityFilter.java
@@ -30,7 +30,7 @@ public class SecurityFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        
+
         var token = this.recoverToken(request);
         if (token != null) {
             var login = tokenService.validateToken(token);
@@ -42,11 +42,13 @@ public class SecurityFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(auth);
                 } catch (Exception e) {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    addCorsHeaders(response);
                     response.getWriter().write("Erro ao validar usuário: " + e.getMessage());
                     return;
                 }
             } else {
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                addCorsHeaders(response);
                 response.getWriter().write("Token inválido ou expirado");
                 return;
             }
@@ -67,4 +69,10 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
         return null;
     }
+
+    private void addCorsHeaders(HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+    }
+    
 }
