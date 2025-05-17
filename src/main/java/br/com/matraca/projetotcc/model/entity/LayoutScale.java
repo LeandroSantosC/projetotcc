a2ed05ee1cfcbef804e4443fe4248afab9b88fb9
Leandro Santos <leandros_cruz@outlook.com>
2025-05-17 19:29:26 -0300

    adicionando construtor vazio no layoutscale

--- src/main/java/br/com/matraca/projetotcc/model/entity/LayoutScale.java
+++ src/main/java/br/com/matraca/projetotcc/model/entity/LayoutScale.java
@@ -3,12 +3,14 @@ package br.com.matraca.projetotcc.model.entity;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Embeddable
 @AllArgsConstructor
+@NoArgsConstructor
 public class LayoutScale {
     private float card;
     private int board;
